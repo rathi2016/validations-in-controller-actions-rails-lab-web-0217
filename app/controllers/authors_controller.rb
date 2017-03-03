db0b6d1312cd71fab6f528c1a555273c02abdc48
github username <github email address>
2017-03-03 15:31:59 -0500

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,12 +4,20 @@ class AuthorsController < ApplicationController
   end
 
   def new
+    @author = Author.new
   end
 
   def create
-    @author = Author.create!(author_params)
+
+ @author= Author.create(author_params)
+
+if   @author.valid?
+    @author.save
 
     redirect_to author_path(@author)
+  else
+    render :new
+  end
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,15 +2,23 @@ class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
-    @post.update(post_params)
+ @post.update(post_params)
+   if  @post.valid?
+
 
     redirect_to post_path(@post)
+  else
+    render :edit
+  end
+
   end
 
   private
